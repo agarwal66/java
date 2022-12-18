cb3d59fe2781cf1d5ed88c85a09b3f1959dbbbca
Prateek Agarwal <114166286+agarwal66@users.noreply.github.com>
2022-12-18 11:52:31 +0530

    Add files via upload

---
+++ index18.js
@@ -0,0 +1,52 @@
+const a=document.getElementById("a1")
+a.innerHTML="hey"
+console.log(a)
+const b=document.getElementById("a2")
+b.innerHTML="my "
+console.log(b)
+const c=document.getElementById("a3")
+c.innerHTML="name"
+console.log(c)
+const d=document.getElementById("a4")
+d.innerHTML="is"
+console.log(d)
+const e=document.getElementById("a5")
+var p= prompt("enter your name here")
+e.innerHTML= p
+console.log(p)
+const f=document.getElementById("a6")
+f.innerHTML="and"
+console.log(f)
+const g=document.getElementById("a7")
+g.innerHTML="my"
+console.log(g)
+const h=document.getElementById("a8")
+h.innerHTML="age"
+console.log(h)
+const i=document.getElementById("a9")
+i.innerHTML="is"
+console.log(i)
+const j=document.getElementById("a10")
+var z= prompt("enter your age here")
+j.innerHTML= z
+console.log(z)
+const k=document.getElementById("a11")
+k.innerHTML="i"
+console.log(k)
+const l=document.getElementById("a12")
+l.innerHTML="love"
+console.log(l)
+const m=document.getElementById("a13")
+m.innerHTML="sub"
+console.log(m)
+const n=document.getElementById("a14")
+var p= prompt("enter your sub here")
+n.innerHTML= p
+console.log(p)
+
+
+
+//const p1Element= document.getElementById("p1")
+//console.log(p1Element)
+//const paragraphTagElement = document.getElementById("paragraphTagElement")
+//console.log(paragraphTagElement)
